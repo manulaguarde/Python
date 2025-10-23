1f29a00ad8922c54d39652082a51253d34968926
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2025-10-23 14:13:47 +0200

    agrego carpeta de ejercicios

---
+++ Boletin_2/Ejercicios1_4.py
@@ -0,0 +1,16 @@
+"""
+1. Escribir un programa que nos pida tres palabras por teclado en cualquier orden y nos
+las muestre en pantalla ordenadas alfabeticamente en orden ascendente
+
+2. Idem al anterior pero ordenando ahora en orden descendente
+
+3. Escribir un programa que pida un número por teclado al usuario que simule ser el
+precio de un artículo y escriba el resultado de aplicarle el IVA del 21%. El resultado
+debe de estar redondeado a dos decimales.
+
+4. Escribir un programa que nos pida por teclado dos calificaciones numéricas de un
+alumno y nos muestre la media aritmética resultante redondeada sin decimales. Las
+notas introducidas deben de estar entre 0 y 10 y admiten decimales. Caso de que una
+entrada sea errónea debería de advertirnos de ello y no hacer el cálculo
+"""
+

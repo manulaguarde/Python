57025a2e205c788c5282d98544f073f4e9a39cc0
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2025-11-06 14:19:28 +0100

    continuo tomando apuntes del contenido nuevo

---
+++ PyCharmMiscProject/Arrays(vectores).py
@@ -0,0 +1,34 @@
+vector=[] #inicializamos un vector, nombre del vector, = , corchetes indica que es array>>vector es sinonimo de array-- este es un vector vacio
+#EN PYTHON SE LLAMAN LISTAS
+vector2 = [1,2,3] #vector con tres numeros enteros
+estudiantes = ["Ana","Pepe","Juan"] #vector de 3 Strings
+
+fichaPersonal = ["Manuel", "Rey Laguarde", 29, True, 1200.45] #puede tener el tipo de dato que quiera
+
+print(fichaPersonal)#puedo mostrar el vector entero
+print(fichaPersonal[2]) #imprime la edad
+
+#COMO RECORRO EL CONTENIDO: POR ELEMENTOS DE LA LISTA
+
+for elemento in fichaPersonal: #elemento es el nombre que le pongo a cada lugar que ocupa el vector
+    print(elemento) #imprime el contenido de cada posicion
+
+#POR POSICION Y ELEMENTO (tengo el elemento y la posicion que ocupa en el array
+
+for i, elemento in enumerate(fichaPersonal): #utilizo la funcion enumerate para  -- tengo que separar las variables con coma
+    print(i, "-", elemento)
+
+#POR POSICION Y ELEMENTO pero con
+
+for i in range(len(fichaPersonal)): #range toma los valores desde el primer valor hasta el ultimo -1 -- es el mas versátil
+    print(i, "-", fichaPersonal) #con range puedo invertir el orden del vector (revisar con cadenas de texto)
+
+for i in range(len(fichaPersonal))
+
+print(fichaPersonal[-1]) #asi me imprime el contenido del ultimo valor de mi array
+
+#MODIFICAR EL ARRAY
+
+fichaPersonal[-1]=1300.50 #cambio el ultimo elemento del array por otro valor, puede ser cualquiera, que tenga que ver o no
+print(fichaPersonal)
+

953ae5cf52aa2e46aade8da464d28755698d7142
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2025-11-07 12:33:46 +0100

    Termino de corregir ejercicio 3 del examen

--- CorreccionExamen/Ejercicio3.py
+++ CorreccionExamen/Ejercicio3.py
@@ -9,7 +9,7 @@ Suponiendo que las dos cadenas de las que se parte son las siguientes:
  texto1 = “Examen 1T01”;
  texto2 = “Octubre-2025”;
 La ejecución de tu programa debería de mostrar esto:
- Resultado: 1T01-2025 Examen Octubre (23)
+ Resultado: 1T01-2025 Examen Octubre (24)
  Como ves, aparecen primero las palabras que van en segundo lugar de ambas separadas por un
 guión y luego las palabras que van en primer lugar separadas por un espacio. Finalmente, un
 paréntesis con la longitud total que suman ambas.
@@ -22,3 +22,18 @@ muestra en consola."""
 
 texto1 = "Examen 1T01"
 texto2 = "Octubre-2025"
+
+#longitud=len(texto1+texto2)
+posicion = texto1.index(" ") #aqui con index hago un "corte" entonces posicion siempre sera hasta el espacio sin importar cuantos caracteres haya antes o despues
+posicionDos = texto2.index("-")
+
+uno= texto1[:posicion+1] #aqui tengo la primera parte del texto, hasta la posicion del espacio
+dos=texto1[posicion+1:] #y aqui  la segunda parte desde posicion+1 (para que no cuente el espacio) hasta el final del texto
+
+tres= texto2[:posicionDos]
+cuatro= texto2[posicionDos:]
+
+resultado=dos+cuatro+" "+uno+tres
+longitud=len(resultado)
+
+print(resultado," (",longitud,")",sep="")

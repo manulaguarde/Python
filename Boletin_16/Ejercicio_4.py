360d99e72b3afaf52b6ef4238c7ea71cd734681b
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2026-01-30 18:21:56 +0100

    Avanzo con el boletín 16

--- Boletin_16/Ejercicio_4.py
+++ Boletin_16/Ejercicio_4.py
@@ -4,27 +4,71 @@ tres victorias. Hazlo de forma modular"""
 
 import random
 
+def ganaOrdenador(ordenador,usuario):
+    if ordenador=="piedra" and usuario=="tijera":
+        return True
+    if ordenador=="tijera" and usuario=="papel":
+        return True
+    if ordenador=="papel" and usuario=="piedra":
+        return True
+    return False
+
+def ganaUsuario(ordenador,usuario):
+    if usuario=="piedra" and ordenador=="tijera":
+        return True
+    if usuario=="tijera" and ordenador=="papel":
+        return True
+    if usuario=="papel" and ordenador=="piedra":
+        return True
+    return False
+
+def ganoPartidaUsuario(ganoUsuario,empate):
+    if ganoUsuario>=2:
+        return True
+    if ganoUsuario==1 and empate==2:
+        return True
+    return False
+
+def ganoPartidaOrdenador(ganoOrdenador,empate):
+    if ganoOrdenador>=2:
+        return True
+    if ganoOrdenador==1 and empate==2:
+        return True
+    return False
+
+def huboEmpate(ganoOrdenador,ganoUsuario,empate):
+    if empate==1 and ganoUsuario==1 and ganoOrdenador==1:
+        return True
+    return False
+    
+
+def batalla():
+    piePapOTij = ("piedra", "papel", "tijera")
+    ganoUsuario=0
+    ganoOrdenador=0
+    empate=0
+    for i in range (3):
+        ordenador = piePapOTij[random.randint(0, 2)]
+        usuario = input("piedra, papel o tijera: ")
+        print("Ordenador: ", ordenador)
+        if ganaOrdenador(ordenador,usuario):
+            ganoOrdenador+=1
+            print("Ordenador gana esta batalla")
+        elif ganaUsuario(ordenador,usuario):
+            ganoUsuario+=1
+            print("Usuario gana esta batalla")
+        else:
+            empate+=1
+            print("empatan esta batalla")
+
+    if ganoPartidaUsuario(ganoUsuario,empate) :
+        print("El usuario gana!")
+    elif ganoPartidaOrdenador(ganoOrdenador,empate):
+        print("El usuario gana!")
+    else:
+        print("Hay empate!")
 
 
-def batalla(ordenador, usuario):
-    piedra = 0
-    papel = 0
-    tijera = 0
-    if ordenador=="piedra"==usuario:
-        piedra=1
-    if ordenador=="papel"==usuario:
-        papel=1
-    if ordenador=="tijera"==usuario:
-        tijera=1
-
-    if usuario>ordenador:
-
-
-
-
-piePapOTij=("piedra","papel","tijera")
-
-ordenador=piePapOTij[random.randint(0,2)]
-usuario=input("piedra, papel o tijera: ")
+batalla()
 
 

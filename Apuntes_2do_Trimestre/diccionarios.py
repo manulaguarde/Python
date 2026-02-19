4f33b3796e6efc79b1120a14974b3d790dbe0227
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2026-02-19 14:17:18 +0100

    añado apuntes de diccionarios

--- Apuntes_2do_Trimestre/diccionarios.py
+++ Apuntes_2do_Trimestre/diccionarios.py
@@ -32,14 +32,89 @@ liga2={'equipo1':[0,0,0,0,0,0,0,0], 'equipo2':[0,0,0,0,0,0,0,0], 'equipo3':[0,0,
 #Como recorreer un diccionario (recordamos que no tienen orden)
 #tengo distintas maneras de recorrerlos, para recuperar solo claves, para recuperar solo valores, y para recuperar ambos
 
+#clave
 for elemento in liga: #así solo recupero las claves del diccionario
     print(elemento)
 
+#valor
 for elemento in liga: #asi recupero los valores
     print(liga[elemento])
 
+#clave y valor
 for clave, valor in liga.items(): #asi recupero ambos, clave y valor
     print(clave,"puntos: ", valor)
+
+#clave y valor
+#(una segunda forma)
+for elemento in liga:
+    print(elemento, "-", liga[elemento])
+
+#podemos obtener una lista con todas las claves del diccionario o bien una lista con todos los valores del diccionario
+
+claves=list(liga.keys())
+valores=list(liga.values())
+
+print(claves)
+print(valores)
+
+#como se añaden elemento a un diccionario, como se modifican y como se eliminan
+
+#MODIFICAR Y AÑADIR UN ELEMENTO (hay que recordar que las claves no pueden estar repetidas,los valores si)
+#si añado un elemento nuevo y la clave de ese elemento ya existe lo que hace es sobreescribir la clave anterior con los valores nuevos
+
+#asi añadimos
+#en caso que no exista lo añade sin problema
+liga['equipo4']=77
+#en caso de que exista sobreescribe el valor
+liga['equipo2']=22
+print(liga)
+
+personas={913285666: "Jose María", 666333444:"Manuel", 9456123564:"María"}
+personas[913285666]="Ines"
+personas[654212354]="Antonio"
+
+print(personas)
+
+telefono=611232456
+personas[telefono]="Antionio"
+print(personas)
+
+#METODO CLEAR BORRA EL DICCIONARIO
+
+liga2.clear()
+print(liga2)
+
+#para borrar SOLO UN ELEMENTO USAMOS POP
+
+liga.pop("equipo2")
+print(liga)
+
+#si el parametro que le paso, osea el elemento no existe me provoca una excepcion
+"""liga.pop("equipo22")"""
+#por otro lado si le agrego un mensaje me imprime ese mensaje
+valor=liga.pop("equipo22", "ese elemento no existe") #ademas pop nos devuelve solo el valor
+print(valor)
+print(liga)
+
+valor=liga.popitem() #POPITEM borra y nos devuelve el último introducido
+                    #pop item nos devuelve la clave y el valor
+print(valor)
+
+#FUCIONAR DICCIONARIOS
+#Las claves duplicadas las sobreescribe por los valores del diccionario añadido (osea del segundo)
+#Las claves que no estén duplicadas nos los añade
+print(liga)
+supercopa={'equipo6':11,'equipo1':55,'equipo4':33}
+
+liga.update(supercopa)
+print(liga)
+
+#puedo tambien copiar el diccionario para luego modificar el nuevo y mantener el diccionario anterior con los valores anteriores
+nuevaliga=liga.copy()
+
+nuevaliga.update(supercopa)
+print(supercopa)
+print(nuevaLiga)
 
 
 

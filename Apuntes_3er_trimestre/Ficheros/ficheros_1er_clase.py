4aa20a88537ec4f30d35e84db2270df213bc20f0
Manuel Rey Laguarde <manuelreylaguarde@gmail.com>
2026-04-20 14:19:00 +0200

    agrego apuntes sobre acceso a ficheros

--- Apuntes_3er_trimestre/Ficheros/ficheros_1er_clase.py
+++ Apuntes_3er_trimestre/Ficheros/ficheros_1er_clase.py
@@ -1,7 +1,7 @@
 #vamos a empezar con leer ficheros
 
 try:
-    cursor=open("/home/alumno/fichero.txt","rt") #abrimos el fichero y cualquier referencia que quiera hacer sobre el fichero utilizaré la variable que creé (cursor)
+   # cursor=open("/home/alumno/fichero.txt","rt") #abrimos el fichero y cualquier referencia que quiera hacer sobre el fichero utilizaré la variable que creé (cursor)
                                 # rt significa el modo en el que estamos trabajando, por defecto es modo lectura y modo texto (rt) pero sino necesitamos especificar el modo
                                 #si pongo el nombre del fichero solamente, el programa buscara el fichero donde se encuentra el programa.
                                 #lo mejor es trabajar con rutas absolutas para no tener problemas
@@ -20,11 +20,23 @@ try:
     """for linea in cursor:
         print(linea,end="")"""
 
-    lista=cursor.readlines() #me lee todas las líneas y lo mete en una lista: transforma cada línea en un elemneto de una lista
-    print(lista)
-    #
+    #otro método más
+    """lista=cursor.readlines() #me lee todas las líneas y lo mete en una lista: transforma cada línea en un elemneto de una lista
+    print(lista)"""
 
-    cursor.close()
+    #cuando readline no lleva argumentos lee una línea completa, pero si le paso por parámetro un número entero lee tantos carácteres como parámetro
+    """caracter = cursor.readline(4)
+    while caracter != "":
+        print(caracter)
+        caracter = cursor.readline(4)"""
+
+    #cursor.close()
+
+    #y este es un método que nos garantiza cerrar el fichero aunque se nos olvide
+    with open("/home/alumno/fichero.txt","rt") as cursor:
+        lista= cursor.readlines()
+        print(lista)
+
 
     #si quiero leer un fichero que no existe me va a dar un error
 except:
